e6ca159ac30a628f0281184b8098a5668ae72a41
HamidOmarr <89842207+HamidOmarr@users.noreply.github.com>
2025-04-09 17:50:51 +0200

    Migrated the correct database to the system.
    -Login function works with the newly implemented database

--- DataAccess/AccountsAccess.cs
+++ DataAccess/AccountsAccess.cs
@@ -5,26 +5,39 @@ using Dapper;
 
 public static class AccountsAccess
 {
-    private static SqliteConnection _connection = new SqliteConnection($"Data Source=DataSources/project.db");
+    private static SqliteConnection _connection = new SqliteConnection($"Data Source=DataSources/Reservationsysteem.db");
 
-    private static string Table = "Accounts";
+    private static string Table = "user";
 
     public static void Write(AccountModel account)
     {
-        string sql = $"INSERT INTO {Table} (email, password, fullname) VALUES (@EmailAddress, @Password, @FullName)";
-        _connection.Execute(sql, account);
+        string sql = $"INSERT INTO {Table} (name, email, password_hash, account_type) VALUES (@Name, @Email, @PasswordHash, @AccountType)";
+        _connection.Execute(sql, new
+        {
+            Name = account.Name,
+            Email = account.Email,
+            PasswordHash = account.PasswordHash,
+            AccountType = account.AccountType
+        });
     }
 
     public static AccountModel GetByEmail(string email)
     {
-        string sql = $"SELECT * FROM {Table} WHERE email = @Email";
+        string sql = $"SELECT id AS Id, name AS Name, email AS Email, password_hash AS PasswordHash, account_type AS AccountType FROM {Table} WHERE email = @Email";
         return _connection.QueryFirstOrDefault<AccountModel>(sql, new { Email = email });
     }
 
     public static void Update(AccountModel account)
     {
-        string sql = $"UPDATE {Table} SET email = @EmailAddress, password = @Password, fullname = @FullName WHERE id = @Id";
-        _connection.Execute(sql, account);
+        string sql = $"UPDATE {Table} SET name = @Name, email = @Email, password_hash = @PasswordHash, account_type = @AccountType WHERE id = @Id";
+        _connection.Execute(sql, new
+        {
+            Id = account.Id,
+            Name = account.Name,
+            Email = account.Email,
+            PasswordHash = account.PasswordHash,
+            AccountType = account.AccountType
+        });
     }
 
     public static void Delete(AccountModel account)
@@ -32,7 +45,4 @@ public static class AccountsAccess
         string sql = $"DELETE FROM {Table} WHERE id = @Id";
         _connection.Execute(sql, new { Id = account.Id });
     }
-
-
-
 }
