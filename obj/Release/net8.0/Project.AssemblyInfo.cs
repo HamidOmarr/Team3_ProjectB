53eeaa11f23d76d773e70ef64b7d59878db4a422
AmarNagim004 <amarnagim@Hotmail.com>
2025-04-10 11:54:35 +0200

    Added functional .gitignore file to avoid unnecessary conflicts

--- obj/Release/net8.0/Project.AssemblyInfo.cs
+++ obj/Release/net8.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b9c1a5aca64184afbe35421392657223ca81313")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6ca159ac30a628f0281184b8098a5668ae72a41")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
