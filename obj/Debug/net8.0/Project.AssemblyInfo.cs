8d54d720f1c4f2de66b4cb1ab52df1405b36145c
AmarNagim004 <amarnagim@Hotmail.com>
2025-04-10 12:08:02 +0200

    Restored Project

--- obj/Debug/net8.0/Project.AssemblyInfo.cs
+++ obj/Debug/net8.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53eeaa11f23d76d773e70ef64b7d59878db4a422")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b2924c6ffd8d49a306542a1e4c5861fff11f8af")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
