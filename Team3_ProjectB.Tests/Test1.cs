a17e9a203bee24781d37900011fcc0ad949078d9
HamidOmarr <89842207+HamidOmarr@users.noreply.github.com>
2025-05-28 21:09:38 +0200

    -added new test project

---
+++ Team3_ProjectB.Tests/Test1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Team3_ProjectB.Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
